224d62a15eb4752c44507ab20c68675009021ac7
d91 <dominik.tomcik23@gmail.com>
2024-06-28 09:00:09 +0200

    wip

---
+++ .changeset/swift-dancers-type.md
@@ -0,0 +1,5 @@
+---
+"motion-styled-components": patch
+---
+
+WIP
--- src/index.tsx
+++ src/index.tsx
@@ -1,78 +1,80 @@
-import type { ReactNode } from "react";
 import React from "react";
-import originalStyled, { type Interpolation } from "styled-components";
-import type { KnownTarget, Styles } from "styled-components/dist/types";
+import originalStyled, {
+  ServerStyleSheet,
+  StyleSheetManager,
+  createGlobalStyle,
+  type StyledInstance,
+  type SupportedHTMLElements,
+} from "styled-components";
+import type { WebTarget } from "styled-components/dist/types";
 import RevealAnimation, {
   type TextAnimationProps,
 } from "./RevealAnimation/RevealAnimation";
-import { elementsSet, type SupportedHTMLElements } from "./domElements";
+import { elementsSet } from "./domElements";
 
-interface StyledRevealProps
-  extends Partial<
-    Omit<TextAnimationProps, "children" | "as" | "style" | "disable">
-  > {
-  children?: ReactNode;
+interface RevealComponentProps
+  extends Omit<TextAnimationProps, "children" | "style" | "as"> {
   animated?: boolean;
-  [key: string]: any;
+  children?: React.ReactNode;
+  [x: string]: any;
 }
 
-const styledBaseReveal = (as: KnownTarget) => {
-  const _StyledElement = originalStyled.span.attrs({ as });
-  return (
-    styles: Styles<object>,
-    ...interpolations: Interpolation<object>[]
-  ) => {
-    const StyledElement = _StyledElement(styles, ...interpolations);
-    const StyledReveal = ({
-      children,
-      animated = false,
-      delay = 0,
-      noSkew = true,
-      duration = 1.2,
-      noCrop = false,
-      y,
-      margin = "0% 0%",
-      once = true,
-      ...props
-    }: StyledRevealProps) => {
-      if (!animated) {
-        return (
-          <StyledElement as={as} {...props}>
-            {children}
-          </StyledElement>
-        );
-      }
+const StyledRevealFactory = <Target extends WebTarget>(tag: Target) => {
+  const RevealComponent = ({
+    children,
+    animated = false,
+    delay = 0,
+    noSkew = true,
+    duration = 1.2,
+    noCrop = false,
+    y,
+    margin = "0% 0%",
+    once = true,
+    ...props
+  }: RevealComponentProps) => {
+    const el = React.createElement(tag, props, children);
+    if (!animated) return el;
 
-      return (
-        <RevealAnimation
-          delay={delay}
-          noSkew={noSkew}
-          duration={duration}
-          noCrop={noCrop}
-          margin={margin}
-          once={once}
-          y={y}
-        >
-          <StyledElement as={as} {...props}>
-            {children}
-          </StyledElement>
-        </RevealAnimation>
-      );
-    };
-
-    StyledReveal.displayName = "StyledReveal";
-    return StyledReveal;
+    return (
+      <RevealAnimation
+        delay={delay}
+        noSkew={noSkew}
+        duration={duration}
+        noCrop={noCrop}
+        y={y}
+        margin={margin}
+        once={once}
+      >
+        {el}
+      </RevealAnimation>
+    );
   };
+
+  const displayName = typeof tag === "string" ? tag : tag.displayName;
+  RevealComponent.displayName = `RevealComponent_${displayName}`;
+
+  return originalStyled(RevealComponent);
 };
 
-type StyledReveal = {
-  [key in SupportedHTMLElements]: ReturnType<typeof styledBaseReveal>;
-};
+type StyledRevealType = {
+  [Key in SupportedHTMLElements]: StyledInstance<
+    "web",
+    Key,
+    JSX.IntrinsicElements[Key] & RevealComponentProps
+  >;
+} & ReturnType<typeof StyledRevealFactory>;
 
-const styled = styledBaseReveal as typeof styledBaseReveal & StyledReveal;
+let styled = StyledRevealFactory as StyledRevealType;
 
-elementsSet.forEach((el) => {
-  styled[el] = styledBaseReveal(el);
+elementsSet.forEach((tag) => {
+  styled[tag] = StyledRevealFactory<typeof tag>(tag);
 });
 
-export default styled;
+export {
+  ServerStyleSheet,
+  StyleSheetManager,
+  createGlobalStyle,
+  type RevealComponentProps,
+  type StyledRevealType,
+  styled as default,
+};
